b255bccb9be2e852df1f3ca7c84baff33f8a5a26
Vijay Yevatkar <es13b1024@iith.ac.in>
2016-07-07 08:41:18 +0530

    Update features.py

--- features.py
+++ features.py
@@ -1,4 +1,4 @@
-Enter file contents here# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Created on Tue Jul 05 17:27:24 2016
 
@@ -7,11 +7,31 @@ Created on Tue Jul 05 17:27:24 2016
 
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+import re
 
-df = pd.read_csv("C:\\Users\\u505123\\Desktop\\Final_SVM.csv")
+#cleanPhrse removes all the unnessary data from the sentences.
+def cleanPhrase(s):
+    s = re.sub(r"[^A-Za-z\']", " ", s)
+    s = re.sub(r"\s{2,}", " ", s)
+    s = re.sub(r" [A-Za-z] ", " ", s)
+    return s.lower().strip()
+
+#Returns a random dataframe after cleaning the data
+def clean(data):
+    phrases = data["Phrase"]
+    new_phrases = []    
+    for i in phrases:
+        x = cleanPhrase(i)
+        new_phrases.append(x)
+    data["Phrase"] = new_phrases
+    return data
+
+#path = "C:\\Users\\u505123\\Desktop\\Final_SVM.csv"
+path = "/home/vjyvtkr/WF/Project/"
+df = pd.read_csv(path+"Final_Achuth.csv")
 test_df = df[0:1874]
 
-sw_file = open("C:/Users/u505123/Documents/Project/ann_text-master/stopwords.txt")    
+sw_file = open(path+"stopwords.txt")    
 stop_words = sw_file.readlines()
 
 for i in range(0,len(stop_words)):
@@ -19,15 +39,20 @@ for i in range(0,len(stop_words)):
 
 sw_file.close()
 
-yes_df = df.loc[df["Indicator"]=="Yes"]
-no_df = df.loc[df["Indicator"]=="No"]
+yes_df = clean(df.loc[df["Indicator"]=="Yes"])
+no_df = clean(df.loc[df["Indicator"]=="No"])
 
-yes_sent = CountVectorizer(stop_words=stop_words,max_features=50)  
-yes_sample = yes_sent.fit_transform(yes_df["Phrase"])  
-no_sent = CountVectorizer(stop_words=stop_words,max_features=200)
-no_sample = no_sent.fit_transform(no_df["Phrase"])
+yes_sent = CountVectorizer(stop_words=stop_words,max_features=100)  
+#yes_sample = yes_sent.fit_transform(yes_df["Phrase"])  
+no_sent = CountVectorizer(stop_words=stop_words,max_features=100)
+#no_sample = no_sent.fit_transform(no_df["Phrase"])
 
 yes_words = yes_sent.get_feature_names()
 no_words = no_sent.get_feature_names()
 feats = yes_words + no_words
 final_feat = list(set(feats))
+obj=open("/home/vjyvtkr/WF/Project/vocab_vj.txt","w+")
+for i in final_feat:
+    obj.write(i)
+    obj.write(",")
+obj.close()
